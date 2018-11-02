3d6bb056b469902fdd162f4ed4ca25a1fde5d349
suin <suinyeze@gmail.com>
2018-11-02 15:03:30 +0900

    fix: fix typo in the description of `--path` option

--- app/cli/app.go
+++ app/cli/app.go
@@ -24,7 +24,7 @@ var App = &cli.App{
 	Flags: []cli.Flag{
 		cli.StringSliceFlag{
 			Name:   "path",
-			Usage:  "Path patterns of git repositories. If this options is specified, The configuration of `remind.paths` in .gitconfig is ignored. You may specify multiple paths like --path=/path/to/a --path=/path/to/b",
+			Usage:  "the path patterns of git repositories. If this options is specified, the git global configuration of `remind.paths` will be ignored. You may specify multiple paths like --path=/path/to/a --path=/path/to/b",
 			EnvVar: "GIT_REMIND_PATHS",
 		},
 	},
