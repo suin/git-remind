7463d5f56f9d9184135509b34048715421183f94
Stefan Dillenburg <stefan.dillenburg@outlook.com>
2022-09-08 10:54:06 +0200

    feat: search repos in a nested project structure

--- infra/filesystem_repos.go
+++ infra/filesystem_repos.go
@@ -1,9 +1,12 @@
 package infra
 
 import (
-	"github.com/suin/git-remind/domain"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/suin/git-remind/domain"
 )
 
 var FilesystemRepos domain.GetReposByPathPattern = func(patterns domain.GetPathPatterns) (repos []string, err error) {
@@ -12,9 +15,17 @@ var FilesystemRepos domain.GetReposByPathPattern = func(patterns domain.GetPathP
 		return
 	}
 	for _, pathPattern := range pathPatterns {
-		paths, err2 := filepath.Glob(string(pathPattern))
-		if err2 != nil {
-			return repos, err2
+		if strings.HasSuffix(string(pathPattern), "/**") {
+			path := strings.TrimSuffix(string(pathPattern), "/**")
+			dirs, err := searchForGitDirs(path)
+			if err != nil {
+				return repos, err
+			}
+			repos = append(repos, dirs...)
+		}
+		paths, err := filepath.Glob(string(pathPattern))
+		if err != nil {
+			return repos, err
 		}
 		for _, path := range paths {
 			isDir, _ := isDirectory(path + "/.git")
@@ -32,4 +43,26 @@ func isDirectory(path string) (bool, error) {
 		return false, err
 	}
 	return fileInfo.IsDir(), err
+}
+
+func searchForGitDirs(root string) ([]string, error) {
+	var skipPath string
+	var matchedPaths []string
+	err := filepath.WalkDir(
+		root,
+		func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if d.IsDir() && d.Name() == ".git" && skipPath != path {
+				skipPath = path
+				matchedPaths = append(matchedPaths, strings.TrimSuffix(path, "/.git"))
+			}
+			return nil
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return matchedPaths, nil
 }
